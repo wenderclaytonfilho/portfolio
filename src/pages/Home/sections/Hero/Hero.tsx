83be4c9dbaeb5e2e3ba658393543525f5b0d65db
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-20 13:49:46 -0300

    fix: Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -12,7 +12,13 @@ const Hero = () => {
     backgroundColor: theme.palette.primary.main,
     height: '100vh',
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+    [theme.breakpoints.up('xs')]:{
+      paddingTop:"100px"
+    },
+    [theme.breakpoints.up("md")]:{
+      paddingTop:"0px"
+    }
   }))
 
   const StyledImage = styled("img")(() => ({
