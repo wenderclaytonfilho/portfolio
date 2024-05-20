947ac4400dc1e6ab9a98faaaaa63fc67726025f0
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-20 13:55:06 -0300

    feat: adds onClick on styledButton

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -49,7 +49,7 @@ const Hero = () => {
               <Grid container display="flex" justifyContent="center" spacing="10" pt={3}>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
 
-                  <StyledButton>
+                  <StyledButton onClick={()=>console.log("download")}>
                     <DownloadIcon />
                     <Typography>
                       Download CV
@@ -57,7 +57,7 @@ const Hero = () => {
                   </StyledButton>
                 </Grid>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton>
+                  <StyledButton onClick={()=>console.log("contact")}>
                     <ContactMailIcon />
                     <Typography>
                       Contact Me
